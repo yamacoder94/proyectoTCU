4a9f0b8d6c0a7f2e647358bd236c83cef02f9375
Yamill Moran <yamacoder94@gmail.com>
2026-08-19 03:35:47 +0200

    second draft,adding option to allow floats as puntajeEscrito

--- models/Proyecto.js
+++ models/Proyecto.js
@@ -1,37 +1,5 @@
 const mongoose = require('mongoose');
 
-/*
-const scoreSchema = new mongoose.Schema({
-  judgeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Juez' },
-  score: { type: Number, required: true },
-  submittedAt: { type: Date, default: Date.now }
-});
-*/
-/*
-const proyectoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  scores: [scoreSchema],
-  latestScore: { type: Number, default: 0 }
-}, { 
-  timestamps: true,
-  collection: 'proyectos' 
-});
-*/
-
-// models/Proyecto.js
-/*
-const proyectoSchema = new mongoose.Schema({
-  tituloProyecto: { type: String, required: true }, // Changed from 'title' to 'tituloProyecto'
-  centroEducativo: { type: String },
-  categoria: { type: String },
-  ejeTematico: { type: String },
-  puntajeTotal: { type: Number },
-  evaluacion: { type: Array, default: [] }
-}, { timestamps: true });
-
-*/
-
 const estudianteSchema = new mongoose.Schema({
   idEstudiante: { type: String, default: "" },
   nombre: { type: String, default: "" }
